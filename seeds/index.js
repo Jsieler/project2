8b5fec24a9a5efb5091d1f910ba5e6475c58dfd4
Jsieler <joshuasieler@Joshs-Air.home>
2021-07-22 22:39:25 -0600

    setup MVC paradigm folder structure

---
+++ seeds/index.js
@@ -0,0 +1,12 @@
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('--------------');
+   
+  
+    process.exit(0);
+  };
+  
+  seedAll();
+  
