fd0c54e88ef1ed2fd1935adbe5c8d6fcb25ca666
Joshua Sieler <80868375+Jsieler@users.noreply.github.com>
2021-07-26 18:00:42 -0600

    Update dashboard-routes.js

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,24 +3,11 @@ const sequelize = require('../config/connection');
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// router.get('/', withAuth, (req, res) => {
-//     console.log(req.session);
-//     console.log('======================');
-//     TBD.findAll({
-//       where: {
-//         user_id: req.session.user_id
-//       },
-      
-//     })
-//       .then(dbPostData => {
-//         const posts = dbPostData.map(post => post.get({ plain: true }));
-//         res.render('dashboard', { TBD, loggedIn: true });
-//       })
-//       .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//       });
-//   });
+router.get('/', withAuth, (req, res) => {
+    res.render('dashboard', {
+      loggedIn: req.session.loggedIn
+    });
+  })
 
 
 module.exports = router;
