79bbc5deeedbed4ec7651a8f55cae805df225bc3
Jsieler <joshuasieler@Joshs-Air.home>
2021-07-26 21:16:00 -0600

    added Quiz and Questions models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,11 @@
 const User = require('./User');
+const Quiz = require('./Quiz');
+const Questions = require('./Questions');
 
+// Questions belongs to Quiz
+Questions.belongsTo(Quiz, {
+    foreignKey: 'quiz_id',
+    onDelete: 'SET NULL'
+  });
 
-
-module.exports = { User };
+module.exports = { User, Quiz, Questions };
